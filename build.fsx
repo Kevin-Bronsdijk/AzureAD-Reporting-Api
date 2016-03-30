629e1c6ccbeed4746c824933061121ca851bd6c8
Kevin Bronsdijk <bronsdijk@bronsdijk.com>
2016-03-30 14:17:30 +0700

    Now building using FAKE
    Added FAKE for managing builds

---
+++ build.fsx
@@ -0,0 +1,70 @@
+// --------------------------------------------------------------------------------------
+// FAKE build script 
+// --------------------------------------------------------------------------------------
+
+#r @"tools\FAKE\tools\FakeLib.dll"
+open Fake
+open Fake.AssemblyInfoFile
+
+// --------------------------------------------------------------------------------------
+// Information about the project to be used at NuGet and in AssemblyInfo files
+// --------------------------------------------------------------------------------------
+
+let project = "Azure AD Reporting Api"
+let authors = ["Kevin Bronsdijk"]
+let summary = "Azure AD Reporting Api"
+let version = "1.0.1.0"
+let description = """
+Azure graph API wrapper for retrieving active directory reporting information.
+"""
+let notes = ""
+let nugetVersion = "1.1.1"
+let tags = "Azure AD Audit Reports Reporting"
+let gitHome = "https://github.com/Kevin-Bronsdijk"
+let gitName = "AzureAD-Reporting-Api"
+
+// --------------------------------------------------------------------------------------
+// Build script 
+// --------------------------------------------------------------------------------------
+
+let buildDir = "./output/"
+
+// --------------------------------------------------------------------------------------
+
+Target "Clean" (fun _ ->
+ CleanDir buildDir
+)
+
+// --------------------------------------------------------------------------------------
+
+Target "AssemblyInfo" (fun _ ->
+    let attributes =
+        [ 
+            Attribute.Title project
+            Attribute.Product project
+            Attribute.Description summary
+            Attribute.Version version
+            Attribute.FileVersion version
+        ]
+
+    CreateCSharpAssemblyInfo "src/AzureADReportingApi/Properties/AssemblyInfo.cs" attributes
+)
+
+// --------------------------------------------------------------------------------------
+
+Target "Build" (fun _ ->
+ !! "src/*.sln"
+ |> MSBuildRelease buildDir "Build"
+ |> Log "AppBuild-Output: "
+)
+
+// --------------------------------------------------------------------------------------
+
+Target "All" DoNothing
+
+"Clean"
+  ==> "AssemblyInfo"
+  ==> "Build"
+  ==> "All"
+
+RunTargetOrDefault "All"
--- src/AzureADReportingApi/Properties/AssemblyInfo.cs
+++ src/AzureADReportingApi/Properties/AssemblyInfo.cs
@@ -2,13 +2,7 @@
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Azure AD Reporting Api")]
 [assembly: AssemblyDescription("Azure AD Reporting Api")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Azure AD Reporting Api")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("ffb7ff1d-8209-4324-9fc5-e922d79ccc9f")]
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
